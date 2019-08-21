7bfaa5ad7120ce3641e80ad939d0a765c41d5b78
Ali Can Karapazar <ackarapazar@gmail.com>
2019-08-21 19:16:46 +0300

    Fetch media insights

--- tap_pream/tasks.py
+++ tap_pream/tasks.py
@@ -38,7 +38,6 @@ def get_ig_user_medias(ig_id):
     return session.send(r.prepare()).json()
 
 
-media_id = "17906226784338300"
 def get_ig_media_metadata(ig_media_id):
     url = fb_graph_url + ig_media_id
     params = {
@@ -49,17 +48,26 @@ def get_ig_media_metadata(ig_media_id):
     return session.send(r.prepare()).json()
 
 
-def get_ig_media_insights():
-    pass
+def get_ig_media_insights(ig_media_id):
+    url = fb_graph_url + ig_media_id + "/insights"
+    params = {
+        "access_token": access_token,
+        "metric": "engagement,impressions,reach"
+    }
+    r = requests.Request(method='GET', url=url, params=params)
+    return session.send(r.prepare()).json()
 
 
 metadata = get_ig_user_metadata(ig_business_id)
 insights = get_ig_user_lifetime_insights(ig_business_id)
 media_objects = get_ig_user_medias(ig_business_id)
-media_metadata = get_ig_media_metadata(media_id)
+media_metadata = get_ig_media_metadata(media_objects['data'][1]['id'])
+media_insights = get_ig_media_insights(media_objects['data'][1]['id'])
 print("metadata: ", "\n", metadata)
 print("insights: ", "\n", insights)
 print("media objects: ", "\n", media_objects)
 print("media metadata :", "\n", media_metadata)
+print("media insights :", "\n", media_insights)
+
 
 
