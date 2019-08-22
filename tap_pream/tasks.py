e849d2d23d376dd957450beef085f235e5fd1d8f
Ali Can Karapazar <ackarapazar@gmail.com>
2019-08-23 00:32:54 +0300

    Create Beat task

--- tap_pream/tasks.py
+++ tap_pream/tasks.py
@@ -1,6 +1,7 @@
 import requests
 from datetime import datetime
 
+from celery import chord, group
 from requests.exceptions import RequestException
 
 from celery_app import app
@@ -9,7 +10,7 @@ from secret import access_token
 
 
 fb_graph_url = "https://graph.facebook.com/v4.0/"
-ig_business_id = "17841401743760151"
+ig_business_ids = ["17841401743760151"]
 
 
 class Client:
@@ -19,6 +20,14 @@ class Client:
 
 
 http_client = Client()
+
+
+@app.task
+def fetch_user_ids():
+    """
+    :return: Instagram user ids
+    """
+    return ig_business_ids
 
 
 @app.task(autoretry_for=(RequestException,), retry_backoff=True, name="instagram.get_user_metadata")
@@ -97,22 +106,26 @@ def get_media_insights(ig_media_id):
 
 @app.task
 def send_data(data):
-    """Send fetched data to Stitch"""
-    pass
-# metadata = get_ig_user_metadata(ig_business_id)
-# insights = get_ig_user_insights(ig_business_id)
-# media_objects = get_ig_user_medias(ig_business_id)
-# media_metadata = get_ig_media_metadata(media_objects['data'][1]['id'])
-# media_insights = get_ig_media_insights(media_objects['data'][1]['id'])
-# print("metadata: ", "\n", metadata)
-# print("insights: ", "\n", insights)
-# print("media objects: ", "\n", media_objects)
-# print("media metadata :", "\n", media_metadata)
-# print("media insights :", "\n", media_insights)
+    """
+    Send data to Stitch Import API
+    :param data: List of data fetched from Instagram API
+    :return: API metrics
+    """
+    return data
 
-@app.task
-def test_task():
-    print(datetime.now())
+
+def update_user_metadata():
+    """
+    Celery task chain to poll user data from IG Graph API and send to Stitch API
+    Logs partial and total execution times on Sentry
+    """
+    user_ids = []  # TODO: fetch user_ids
+    # TODO: add error logging: apply_sync(link_error=e)
+    metadata_chord = chord(group(get_user_metadata.s(user_id) for user_id in user_ids), send_data.s())()
+    insights_chord = chord(group(get_user_insights.s(user_id) for user_id in user_ids), send_data.s())()
+    # TODO: log metadata_chord
+    # TODO: log insights_chord
+
 
 
 
