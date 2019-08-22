7803240b7c0c3d7b89309751867b675bb181336c
Ali Can Karapazar <ackarapazar@gmail.com>
2019-08-22 22:19:47 +0300

    Add task names

--- tap_pream/tasks.py
+++ tap_pream/tasks.py
@@ -11,27 +11,29 @@ from secret import access_token
 fb_graph_url = "https://graph.facebook.com/v4.0/"
 ig_business_id = "17841401743760151"
 
-session = requests.Session()
-
 
 class Client:
+
     def __init__(self):
         self.session = requests.Session()
 
 
-@app.task(autoretry_for=(RequestException,), retry_backoff=True)
-def get_ig_user_metadata(ig_id):
+http_client = Client()
+
+
+@app.task(autoretry_for=(RequestException,), retry_backoff=True, name="instagram.get_user_metadata")
+def get_user_metadata(ig_id):
     url = fb_graph_url + ig_id
     params = {
         "access_token": access_token,
         "fields": "followers_count,media_count"
     }
     r = requests.Request(method='GET', url=url, params=params)
-    return session.send(r.prepare()).json()
+    return http_client.session.send(r.prepare()).json()
 
 
-@app.task(autoretry_for=(RequestException,), retry_backoff=True)
-def get_ig_user_lifetime_insights(ig_id):
+@app.task(autoretry_for=(RequestException,), retry_backoff=True, name="instagram.get_user_insights")
+def get_user_insights(ig_id):
     url = fb_graph_url + ig_id + "/insights"
     params = {
         "access_token": access_token,
@@ -39,43 +41,43 @@ def get_ig_user_lifetime_insights(ig_id):
         "period": "lifetime"
     }
     r = requests.Request(method='GET', url=url, params=params)
-    return session.send(r.prepare()).json()
+    return http_client.session.send(r.prepare()).json()
 
 
-@app.task(autoretry_for=(RequestException,), retry_backoff=True)
-def get_ig_user_medias(ig_id):
+@app.task(autoretry_for=(RequestException,), retry_backoff=True, name="instagram.get_user_medias")
+def get_user_medias(ig_id):
     url = fb_graph_url + ig_id + "/media"
     params = {
         "access_token": access_token,
     }
     r = requests.Request(method='GET', url=url, params=params)
-    return session.send(r.prepare()).json()
+    return http_client.session.send(r.prepare()).json()
 
 
-@app.task(autoretry_for=(RequestException,), retry_backoff=True)
-def get_ig_media_metadata(ig_media_id):
+@app.task(autoretry_for=(RequestException,), retry_backoff=True, name="instagram.get_media_metadata")
+def get_media_metadata(ig_media_id):
     url = fb_graph_url + ig_media_id
     params = {
         "access_token": access_token,
         "fields": "caption,comments_count,like_count,media_type"
     }
     r = requests.Request(method='GET', url=url, params=params)
-    return session.send(r.prepare()).json()
+    return http_client.session.send(r.prepare()).json()
 
 
-@app.task(autoretry_for=(RequestException,), retry_backoff=True)
-def get_ig_media_insights(ig_media_id):
+@app.task(autoretry_for=(RequestException,), retry_backoff=True, name="instagram.get_media_insights")
+def get_media_insights(ig_media_id):
     url = fb_graph_url + ig_media_id + "/insights"
     params = {
         "access_token": access_token,
         "metric": "engagement,impressions,reach"
     }
     r = requests.Request(method='GET', url=url, params=params)
-    return session.send(r.prepare()).json()
+    return http_client.session.send(r.prepare()).json()
 
 
 # metadata = get_ig_user_metadata(ig_business_id)
-# insights = get_ig_user_lifetime_insights(ig_business_id)
+# insights = get_ig_user_insights(ig_business_id)
 # media_objects = get_ig_user_medias(ig_business_id)
 # media_metadata = get_ig_media_metadata(media_objects['data'][1]['id'])
 # media_insights = get_ig_media_insights(media_objects['data'][1]['id'])
