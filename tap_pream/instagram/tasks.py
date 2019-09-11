9c6ba9825488cde882ec62ea262060cc9c2d69c3
Ali Can Karapazar <ackarapazar@gmail.com>
2019-09-11 16:32:24 +0300

    Retry tasks with http error other than 400

--- tap_pream/instagram/tasks.py
+++ tap_pream/instagram/tasks.py
@@ -3,7 +3,7 @@ from requests.exceptions import RequestException
 from datetime import datetime, timezone
 
 from celery import chord, group
-from requests.exceptions import RequestException
+from requests.exceptions import RequestException, Timeout
 
 from celery_app import app
 from secret import access_token
@@ -13,29 +13,13 @@ fb_graph_url = "https://graph.facebook.com/v4.0/"
 ig_business_ids = ["17841401743760151"]
 
 
-class Client:
-
-    def __init__(self):
-        self.session = requests.Session()
-
-    def request(self, method, url, params):
-        req = requests.Request(method=method, url=url, params=params)
-        res = self.session.send(req.prepare())
-        if not res.ok and 500 <= res.status_code < 600:
-            res.raise_for_status()
-        return res.json()
-
-
-http_client = Client()
-
-
 def add_timestamp(data):
     now = datetime.now(timezone.utc)
     data['server_time'] = now.strftime("%Y-%m-%d %H:%M:%S %Z")
     return data
 
 
-@app.task(autoretry_for=(RequestException,), retry_backoff=1, name="instagram.get-user-metadata")
+@app.task(max_retries=3, retry_backoff=1, name="instagram.get-user-metadata")
 def get_user_metadata(ig_id):
     """
     Get Instagram user metadata
@@ -45,28 +29,20 @@ def get_user_metadata(ig_id):
         "access_token": access_token,
         "fields": "id,ig_id,followers_count,media_count"
     }
-    data = http_client.request(method="GET", url=url, params=params)
+
+    r = requests.get(url=url, params=params)
+    if not r.ok:
+        if r.status_code == 400:
+            # TODO: log 400 error
+            raise
+        else:
+            raise get_user_metadata.retry()
+    data = r.json()
     data = add_timestamp(data)
     return data
 
 
-@app.task(autoretry_for=(RequestException,), retry_backoff=1, name="instagram.get-user-insights")
-def get_user_insights(ig_id):
-    """
-    Get insights of Instagram user
-    """
-    url = fb_graph_url + ig_id + "/insights"
-    params = {
-        "access_token": access_token,
-        "metric": "audience_gender_age",
-        "period": "lifetime"
-    }
-    data = http_client.request(method="GET", url=url, params=params)
-    data = add_timestamp(data)
-    return data
-
-
-@app.task(autoretry_for=(RequestException,), retry_backoff=1, name="instagram.get-user-medias")
+@app.task(max_retries=3, retry_backoff=1, name="instagram.get-user-medias")
 def get_user_medias(ig_id):
     """
     Get media objects of Instagram user
@@ -75,13 +51,20 @@ def get_user_medias(ig_id):
     params = {
         "access_token": access_token,
     }
-    data = http_client.request(method="GET", url=url, params=params)
+    r = requests.get(url=url, params=params)
+    if not r.ok:
+        if r.status_code == 400:
+            # TODO: log 400 error
+            raise
+        else:
+            raise get_user_metadata.retry()
+    data = r.json()
     data = add_timestamp(data)
     return data
 
 
-@app.task(autoretry_for=(RequestException,), retry_backoff=1, name="instagram.get-media-metadata")
-def get_media_metadata(ig_media_id):
+@app.task(max_retries=3, retry_backoff=1, name="instagram.get-post-metadata")
+def get_post_metadata(ig_media_id):
     """
     Get metadata of an Instagram media object
     """
@@ -90,13 +73,20 @@ def get_media_metadata(ig_media_id):
         "access_token": access_token,
         "fields": "id,_ig_id,caption,comments_count,like_count,media_type"
     }
-    data = http_client.request(method="GET", url=url, params=params)
+    r = requests.get(url=url, params=params)
+    if not r.ok:
+        if r.status_code == 400:
+            # TODO: log 400 error
+            raise
+        else:
+            raise get_user_metadata.retry()
+    data = r.json()
     data = add_timestamp(data)
     return data
 
 
-@app.task(autoretry_for=(RequestException,), retry_backoff=1, name="instagram.get-media-insights")
-def get_media_insights(ig_media_id):
+@app.task(max_retries=3, retry_backoff=1, name="instagram.get-post-insights")
+def get_post_insights(ig_media_id):
     """
     Get insights of an Instagram media object
     """
@@ -105,13 +95,20 @@ def get_media_insights(ig_media_id):
         "access_token": access_token,
         "metric": "engagement,impressions,reach"
     }
-    data = http_client.request(method="GET", url=url, params=params)
+    r = requests.get(url=url, params=params)
+    if not r.ok:
+        if r.status_code == 400:
+            # TODO: log 400 error
+            raise
+        else:
+            raise get_user_metadata.retry()
+    data = r.json()
     data = add_timestamp(data)
     return data
 
 
 @app.task(name="instagram.target-stitch")
-def target_redshift(data):
+def target_stitch(data, table_name):
     """
     Send data to Stitch Import API
     Max 4 MB or 10.000 data points
@@ -127,16 +124,16 @@ def update_user_data():
     Logs partial and total execution times on Sentry
     """
     user_ids = []  # TODO: fetch user_ids
-    # TODO: add error logging: apply_sync(link_error=e)
-    metadata_tasks = chord(group(get_user_metadata.s(user_id) for user_id in user_ids), target_redshift.s())
-    insights_tasks = chord(group(get_user_insights.s(user_id) for user_id in user_ids), target_redshift.s())
-    group(metadata_tasks, insights_tasks).apply_async()
-    # TODO: log metadata_chord
-    # TODO: log insights_chord
+    metadata = chord(group(get_user_metadata.s(user_id) for user_id in user_ids), target_stitch.s("user_metadata"))
+    metadata.apply_async()
 
 
-def update_media_data():
-    pass
+@app.task(name="instagram.update_post_data")
+def update_post_data():
+    post_ids = []
+    metadata = chord(group(get_post_metadata.s(post_id) for post_id in post_ids), target_stitch.s("post_metadata"))
+    insights = chord(group(get_post_insights.s(post_id) for post_id in post_ids), target_stitch.s("post_insights"))
+    group(metadata, insights).apply_async()
 
 
 @app.task(autoretry_for=(RequestException,), retry_backoff=1)
